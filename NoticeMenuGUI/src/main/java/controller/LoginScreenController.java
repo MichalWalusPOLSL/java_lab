cd33ae408743d092d1918ecd1c49b66eba3345a5
michal.walus <mw306757@student.polsl.pl>
2024-10-26 16:40:39 +0200

    add-app-to-separate-file/lab2

--- NoticeMenuGUI/src/main/java/controller/LoginScreenController.java
+++ NoticeMenuGUI/src/main/java/controller/LoginScreenController.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mycompany.noticemenugui;
+package controller;
 
+import com.mycompany.noticemenugui.App;
 import com.mycompany.noticemenugui.App;
 import java.io.IOException;
 import java.net.URL;
--- NoticeMenuGUI/src/main/java/module-info.java
+++ NoticeMenuGUI/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.mycompany.noticemenugui {
     requires java.base;
 
     opens com.mycompany.noticemenugui to javafx.fxml;
+    opens controller to javafx.fxml;
     opens model to javafx.base;
     exports com.mycompany.noticemenugui;
 }
